4748180bd9f3011d81127de201e5530960c5dc3a
kkellyin90 <keith.kelly.a@gmail.com>
2016-06-25 15:02:12 +0530

    Contacts

---
+++ Session5Assignment2/app/src/main/java/com/funapp/session5assignment2/ContactsActivity.java
@@ -0,0 +1,31 @@
+package com.funapp.session5assignment2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class ContactsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+
+// open contacts from phone
+        Button open = (Button)findViewById(R.id.buttonCall);
+        open.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(ContactsContract.Contacts.CONTENT_URI);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
